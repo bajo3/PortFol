47a1109f10ffa238d232e71a19266b4520194895
Felipe Lentini <99766298+bajo3@users.noreply.github.com>
2023-10-10 13:59:30 -0300

    On loader y mas

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+    // Oculta el loader una vez que la página se haya cargado completamente.
+    const loader = document.getElementById("loader");
+    loader.style.display = "none";
+});
